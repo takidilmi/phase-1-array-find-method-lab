186c7e0bc7a27254ce5c6f73ab51c5c6a9a4580e
takidilmi <angeldilmi@gmail.com>
2023-08-06 15:03:10 +0200

    Compelted assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+/* 
+function superbowlWin(superbowls) {
+    const winningYear = superbowls.find((game) => game.result === "W");
+  
+    return winningYear ? winningYear.year : undefined;
+  } */
+  
+  const superbowlWin = (superbowls) => (superbowls.find((game) => game.result === "W") || {}).year;
